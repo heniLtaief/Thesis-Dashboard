6cb3368fd4634f6621218171c909baf10cf0c997
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-02 16:40:16 +0100

    clean version of dashboard

--- bycycle-dashboard/src/index.js
+++ bycycle-dashboard/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+
+import Themes from "./themes";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { LayoutProvider } from "./context/LayoutContext";
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <LayoutProvider>
+    <UserProvider>
+      <ThemeProvider theme={Themes.default}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </UserProvider>
+  </LayoutProvider>,
+  document.getElementById("root"),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+serviceWorker.unregister();

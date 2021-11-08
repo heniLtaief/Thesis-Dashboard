b019653d1a25c2e7ee4384aa49dc08ba4c6aa4e8
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-08 23:24:18 +0100

    suggestion table created and working

---
+++ bycycle-dashboard/src/pages/tables/BicycleTable.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import MUIDataTable from "mui-datatables";
+
+// components
+import PageTitle from "../../components/PageTitle";
+import axios from "axios";
+const useStyles = makeStyles((theme) => ({
+  tableOverflow: {
+    overflow: "auto",
+  },
+}));
+
+export default function BicycleTable() {
+  const [Bicycles, SetBicycles] = useState([]);
+  var bicycleTable = Bicycles.map(function (obj) {
+    return Object.keys(obj)
+      .sort()
+      .map(function (key) {
+        return obj[key];
+      });
+  });
+  const getAllBicycles = () => {
+    axios
+      .get(`http://localhost:3002/bicycle`)
+      .then((response) => {
+        SetBicycles(response.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  useEffect(() => {
+    getAllBicycles();
+  }, []);
+
+  return (
+    <>
+      <PageTitle title="Bicycles" />
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title="Bicycle List"
+            data={bicycleTable}
+            columns={[
+              "To be removed",
+              "Bike Id",
+              "Category",
+              "Purchase Date",
+              "Description",
+            ]}
+            options={{
+              filterType: "checkbox",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
--- bycycle-dashboard/src/pages/tables/StationTable.js
+++ bycycle-dashboard/src/pages/tables/StationTable.js
@@ -2,27 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import MUIDataTable from "mui-datatables";
-import BicycleTable from "./BicycleTable";
-import StationTable from "./StationTable";
-import SuggestionTable from "./SuggestionTable";
 
 // components
 import PageTitle from "../../components/PageTitle";
-// import Widget from "../../components/Widget";
-// import Table from "../dashboard/components/Table/Table";
-
-// data
-// import mock from "../dashboard/mock";
 import axios from "axios";
-
 const useStyles = makeStyles((theme) => ({
   tableOverflow: {
     overflow: "auto",
   },
 }));
 
-export default function Tables() {
-  const classes = useStyles();
+export default function StationTable() {
   const [Stations, SetStations] = useState([]);
   var stationTable = Stations.map(function (obj) {
     return Object.keys(obj)
@@ -48,25 +38,30 @@ export default function Tables() {
 
   return (
     <>
-      <SuggestionTable></SuggestionTable>
-      <StationTable></StationTable>
-      <BicycleTable></BicycleTable>
+      <PageTitle title="Stations" />
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title="Station List"
+            data={stationTable}
+            columns={[
+              "Number of bikes",
+              "City",
+              "Email",
+              "Income",
+              "Status",
+              "Name",
+              "Situation",
+              "StationId",
+              "CreatedA",
+              "UpdatedAt",
+            ]}
+            options={{
+              filterType: "checkbox",
+            }}
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
-
-/* 
-          {
-          <Grid item xs={12}>
-
-          <Widget
-            title="Incoming Stats By station"
-            upperTitle
-            noBodyPadding
-            bodyClass={classes.tableOverflow}
-          >
-            <Table data={mock.table} />
-          </Widget>
-         </Grid>
-        }
-        */

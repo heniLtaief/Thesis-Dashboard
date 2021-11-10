058bb23d5c228949a6f0cbd89cb8ed8b956bbd79
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-10 11:25:03 +0100

    reviews retreived

---
+++ bycycle-dashboard/src/pages/tables/ReviewsTable.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import MUIDataTable from "mui-datatables";
+
+// components
+import PageTitle from "../../components/PageTitle";
+import axios from "axios";
+const useStyles = makeStyles((theme) => ({
+  tableOverflow: {
+    overflow: "auto",
+  },
+}));
+
+export default function ReviewTable() {
+  const [reviews, SetReviews] = useState([]);
+  var reviewTable = reviews.map(function (obj) {
+    return Object.keys(obj)
+      .sort()
+      .map(function (key) {
+        return obj[key];
+      });
+  });
+  const GetAllreviews = () => {
+    axios
+      .get(`https://bycyclethesis.herokuapp.com/reviews`)
+      .then((response) => {
+        SetReviews(response.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  useEffect(() => {
+    GetAllreviews();
+  }, []);
+
+  return (
+    <>
+      <PageTitle title="reviews" />
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title="review List"
+            data={reviewTable}
+            columns={["User", "ReviewId", "Review Date", "Rating", "review"]}
+            options={{
+              filterType: "checkbox",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}

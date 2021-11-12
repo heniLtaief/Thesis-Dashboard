7e004cf63b86bcc506b8e94d5c8eca40830e4b77
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-12 20:18:18 +0100

    auth working but not redirecting

--- bycycle-dashboard/src/pages/login/Login.js
+++ bycycle-dashboard/src/pages/login/Login.js
@@ -98,239 +98,217 @@ function Login(props) {
       });
   };
   console.log(adminLoggedIn);
-  {
-    if (!adminLoggedIn) {
-      return (
-        <Grid container className={classes.container}>
-          <div className={classes.logotypeContainer}>
-            <img src={bg} alt="bg" className={classes.logotypeImage} />
-          </div>
-          <div className={classes.formContainer}>
-            <div className={classes.form}>
-              <Tabs
-                value={activeTabId}
-                onChange={(e, id) => setActiveTabId(id)}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-              >
-                <Tab label="Login" classes={{ root: classes.tab }} />
-                <Tab label="Register" classes={{ root: classes.tab }} />
-              </Tabs>
-              {activeTabId === 0 && (
-                <React.Fragment>
-                  <TextField
-                    id="email"
-                    InputProps={{
-                      classes: {
-                        underline: classes.textFieldUnderline,
-                        input: classes.textField,
-                      },
-                    }}
-                    // value={loginValue}
-                    name="Username"
-                    onChange={handleLogin}
-                    margin="normal"
-                    placeholder="Username"
-                    type="text"
-                    fullWidth
-                  />
-                  <TextField
-                    id="password"
-                    name="LoginPassword"
-                    InputProps={{
-                      classes: {
-                        underline: classes.textFieldUnderline,
-                        input: classes.textField,
-                      },
-                    }}
-                    // value={passwordValue}
-                    onChange={handleLogin}
-                    margin="normal"
-                    placeholder="Password"
-                    type="password"
-                    fullWidth
-                  />
-                  <div className={classes.formButtons}>
-                    {isLoading ? (
-                      <CircularProgress
-                        size={26}
-                        className={classes.loginLoader}
-                      />
-                    ) : (
-                      <Button
-                        // disabled={
-                        //   LoginAdmin.User.length === 0 ||
-                        //   LoginAdmin.Password.length === 0
-                        // }
-                        onClick={
-                          () => {
-                            checkAdmin();
-                          }
-                          //   // () =>
-                          //   //   loginUser(
-                          //   //     userDispatch,
-                          //   //     loginValue,
-                          //   //     passwordValue,
-                          //   //     props.history,
-                          //   //     setIsLoading,
-                          //   //     setError,
-                          //   //   )
-                        }
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                      >
-                        Login
-                      </Button>
-                    )}
-                    <Button
-                      color="primary"
-                      size="large"
-                      className={classes.forgetButton}
-                    >
-                      Forget Password
-                    </Button>
-                  </div>
-                  <div className={classes.formDividerContainer}>
-                    <div className={classes.formDivider} />
-                    <Typography className={classes.formDividerWord}>
-                      or
-                    </Typography>
-                    <div className={classes.formDivider} />
-                  </div>
-                  <Button size="large" className={classes.googleButton}>
-                    <img
-                      src={google}
-                      alt="google"
-                      className={classes.googleIcon}
-                    />
-                    &nbsp;Sign in with Google
-                  </Button>
-                </React.Fragment>
-              )}
-              {activeTabId === 1 && (
-                <React.Fragment>
-                  <Fade in={error}>
-                    <Typography
-                      color="secondary"
-                      className={classes.errorMessage}
-                    >
-                      Something is wrong with your login or password :(
-                    </Typography>
-                  </Fade>
-                  <TextField
-                    id="name"
-                    InputProps={{
-                      classes: {
-                        underline: classes.textFieldUnderline,
-                        input: classes.textField,
-                      },
-                    }}
-                    // value={nameValue}
-                    name="User"
-                    onChange={handleRegister}
-                    margin="normal"
-                    placeholder="Full Name"
-                    type="text"
-                    fullWidth
-                  />
-                  <TextField
-                    name="Email"
-                    id="email"
-                    InputProps={{
-                      classes: {
-                        underline: classes.textFieldUnderline,
-                        input: classes.textField,
-                      },
-                    }}
-                    // value={loginValue}
-                    onChange={handleRegister}
-                    margin="normal"
-                    placeholder="Email Adress"
-                    type="email"
-                    fullWidth
-                  />
-                  <TextField
-                    id="password"
-                    name="Password"
-                    InputProps={{
-                      classes: {
-                        underline: classes.textFieldUnderline,
-                        input: classes.textField,
-                      },
-                    }}
-                    // value={passwordValue}
-                    onChange={handleRegister}
-                    margin="normal"
-                    placeholder="Password"
-                    type="password"
-                    fullWidth
-                  />
-                  <div className={classes.creatingButtonContainer}>
-                    {isLoading ? (
-                      <CircularProgress size={26} />
-                    ) : (
-                      <Button
-                        onClick={() => {
-                          console.log(RegisterAdmin);
-
-                          // loginUser(
-                          //   userDispatch,
-                          //   loginValue,
-                          //   passwordValue,
-                          //   props.history,
-                          //   setIsLoading,
-                          //   setError,
-                          // );
-                        }}
-                        // disabled={
-                        //   RegisterAdmin.User.length === 0 ||
-                        //   RegisterAdmin.Password.length === 0 ||
-                        //   RegisterAdmin.Email.length === 0
-                        // }
-                        onClick={() => {
-                          registerAdmin();
-                        }}
-                        size="large"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        className={classes.createAccountButton}
-                      >
-                        Create your account
-                      </Button>
-                    )}
-                  </div>
-                  <div className={classes.formDividerContainer}>
-                    <div className={classes.formDivider} />
-                    <Typography className={classes.formDividerWord}>
-                      or
-                    </Typography>
-                    <div className={classes.formDivider} />
-                  </div>
+  return (
+    <Grid container className={classes.container}>
+      <div className={classes.logotypeContainer}>
+        <img src={bg} alt="bg" className={classes.logotypeImage} />
+      </div>
+      <div className={classes.formContainer}>
+        <div className={classes.form}>
+          <Tabs
+            value={activeTabId}
+            onChange={(e, id) => setActiveTabId(id)}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="Login" classes={{ root: classes.tab }} />
+            <Tab label="Register" classes={{ root: classes.tab }} />
+          </Tabs>
+          {activeTabId === 0 && (
+            <React.Fragment>
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                // value={loginValue}
+                name="Username"
+                onChange={handleLogin}
+                margin="normal"
+                placeholder="Username"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                name="LoginPassword"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                // value={passwordValue}
+                onChange={handleLogin}
+                margin="normal"
+                placeholder="Password"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.formButtons}>
+                {isLoading ? (
+                  <CircularProgress size={26} className={classes.loginLoader} />
+                ) : (
                   <Button
+                    // disabled={
+                    //   LoginAdmin.User.length === 0 ||
+                    //   LoginAdmin.Password.length === 0
+                    // }
+                    onClick={
+                      () => {
+                        checkAdmin();
+                      }
+                      //   // () =>
+                      //   //   loginUser(
+                      //   //     userDispatch,
+                      //   //     loginValue,
+                      //   //     passwordValue,
+                      //   //     props.history,
+                      //   //     setIsLoading,
+                      //   //     setError,
+                      //   //   )
+                    }
+                    variant="contained"
+                    color="primary"
                     size="large"
-                    className={classnames(
-                      classes.googleButton,
-                      classes.googleButtonCreating,
-                    )}
                   >
-                    <img
-                      src={google}
-                      alt="google"
-                      className={classes.googleIcon}
-                    />
-                    &nbsp;Sign in with Google
+                    Login
                   </Button>
-                </React.Fragment>
-              )}
-            </div>
-          </div>
-        </Grid>
-      );
-    }
-  }
+                )}
+                <Button
+                  color="primary"
+                  size="large"
+                  className={classes.forgetButton}
+                >
+                  Forget Password
+                </Button>
+              </div>
+              <div className={classes.formDividerContainer}>
+                <div className={classes.formDivider} />
+                <Typography className={classes.formDividerWord}>or</Typography>
+                <div className={classes.formDivider} />
+              </div>
+              <Button size="large" className={classes.googleButton}>
+                <img src={google} alt="google" className={classes.googleIcon} />
+                &nbsp;Sign in with Google
+              </Button>
+            </React.Fragment>
+          )}
+          {activeTabId === 1 && (
+            <React.Fragment>
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="name"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                // value={nameValue}
+                name="User"
+                onChange={handleRegister}
+                margin="normal"
+                placeholder="Full Name"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                name="Email"
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                // value={loginValue}
+                onChange={handleRegister}
+                margin="normal"
+                placeholder="Email Adress"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                name="Password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                // value={passwordValue}
+                onChange={handleRegister}
+                margin="normal"
+                placeholder="Password"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.creatingButtonContainer}>
+                {isLoading ? (
+                  <CircularProgress size={26} />
+                ) : (
+                  <Button
+                    onClick={() => {
+                      console.log(RegisterAdmin);
+
+                      // loginUser(
+                      //   userDispatch,
+                      //   loginValue,
+                      //   passwordValue,
+                      //   props.history,
+                      //   setIsLoading,
+                      //   setError,
+                      // );
+                    }}
+                    // disabled={
+                    //   RegisterAdmin.User.length === 0 ||
+                    //   RegisterAdmin.Password.length === 0 ||
+                    //   RegisterAdmin.Email.length === 0
+                    // }
+                    onClick={() => {
+                      registerAdmin();
+                    }}
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    className={classes.createAccountButton}
+                  >
+                    Create your account
+                  </Button>
+                )}
+              </div>
+              <div className={classes.formDividerContainer}>
+                <div className={classes.formDivider} />
+                <Typography className={classes.formDividerWord}>or</Typography>
+                <div className={classes.formDivider} />
+              </div>
+              <Button
+                size="large"
+                className={classnames(
+                  classes.googleButton,
+                  classes.googleButtonCreating,
+                )}
+              >
+                <img src={google} alt="google" className={classes.googleIcon} />
+                &nbsp;Sign in with Google
+              </Button>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </Grid>
+  );
 }
 
 export default Login;

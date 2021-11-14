724686db8aaa556c908ce38895f7a36093419de3
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-14 09:59:30 +0100

    planneer view with dummy data implemented

--- bycycle-dashboard/src/pages/tables/planner.js
+++ bycycle-dashboard/src/pages/tables/planner.js
@@ -3,34 +3,80 @@ import Paper from "@material-ui/core/Paper";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
-  DayView,
+  WeekView,
   Appointments,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { makeStyles } from "@material-ui/core/styles";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import appointments from "./demo-data/today-appointments";
 
-const currentDate = "2018-11-01";
-const schedulerData = [
-  {
-    startDate: "2018-11-01T09:45",
-    endDate: "2018-11-01T11:00",
-    title: "Meeting",
+const useStyles = makeStyles((theme) => ({
+  todayCell: {
+    backgroundColor: fade(theme.palette.primary.main, 0.1),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.primary.main, 0.14),
+    },
+    "&:focus": {
+      backgroundColor: fade(theme.palette.primary.main, 0.16),
+    },
   },
-  {
-    startDate: "2018-11-01T12:00",
-    endDate: "2018-11-01T13:30",
-    title: "Go to a gym",
+  weekendCell: {
+    backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    },
+    "&:focus": {
+      backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    },
   },
-];
+  today: {
+    backgroundColor: fade(theme.palette.primary.main, 0.16),
+  },
+  weekend: {
+    backgroundColor: fade(theme.palette.action.disabledBackground, 0.06),
+  },
+}));
 
-export default function Planner() {
-  return (
-    <div>
-      <Paper>
-        <Scheduler data={schedulerData}>
-          <ViewState currentDate={currentDate} />
-          <DayView startDayHour={9} endDayHour={14} />
-          <Appointments />
-        </Scheduler>
-      </Paper>
-    </div>
-  );
-}
+const TimeTableCell = (props) => {
+  const classes = useStyles();
+  const { startDate } = props;
+  const date = new Date(startDate);
+
+  if (date.getDate() === new Date().getDate()) {
+    return <WeekView.TimeTableCell {...props} className={classes.todayCell} />;
+  }
+  if (date.getDay() === 0 || date.getDay() === 6) {
+    return (
+      <WeekView.TimeTableCell {...props} className={classes.weekendCell} />
+    );
+  }
+  return <WeekView.TimeTableCell {...props} />;
+};
+
+const DayScaleCell = (props) => {
+  const classes = useStyles();
+  const { startDate, today } = props;
+
+  if (today) {
+    return <WeekView.DayScaleCell {...props} className={classes.today} />;
+  }
+  if (startDate.getDay() === 0 || startDate.getDay() === 6) {
+    return <WeekView.DayScaleCell {...props} className={classes.weekend} />;
+  }
+  return <WeekView.DayScaleCell {...props} />;
+};
+
+export default () => (
+  <Paper>
+    <Scheduler data={appointments} height={660}>
+      <ViewState />
+      <WeekView
+        startDayHour={9}
+        endDayHour={19}
+        timeTableCellComponent={TimeTableCell}
+        dayScaleCellComponent={DayScaleCell}
+      />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+);
